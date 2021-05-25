282bbe106d6ff0dadc3f2a5f1ce5eb1500019814
Pedro <pemoraes1@gmail.com>
2021-05-25 02:35:05 -0300

    Corrige style do Card de Partidas

--- src/components/InfoPartida.vue
+++ src/components/InfoPartida.vue
@@ -29,7 +29,7 @@ export default {
 
 <style scoped>
 #card {
-    width: 25%;
+    width: 50%;
     display: flex;
     flex-direction: row;
     margin: 2% auto;
@@ -39,17 +39,5 @@ export default {
 }
 .info {
     text-align: center;
-}
-
-input {
-    width: 40%;
-    display: flex;
-    margin: 5% auto 0;
-}
-button {
-    width: 20%;
-    display: flex;
-    margin: 1% auto 0;
-    border-radius: 30px;
 }
 </style>
